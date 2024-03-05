b0954f910c33d562ecf236cfa84d4517ccb21e37
koki-develop <kou.pg.0131@gmail.com>
2024-03-05 20:47:18 +0900

    Refactor if statements and add increment/decrement example

--- internal/mingo/mingo_test.go
+++ internal/mingo/mingo_test.go
@@ -14,7 +14,7 @@ func Test_Minify(t *testing.T) {
 		want string
 	}{
 		{
-			name: "if statements",
+			name: "if",
 			src: `package main
 
 import "fmt"
@@ -57,6 +57,22 @@ func run2() (string, error) {
 }
 `,
 			want: `package main;import "fmt";func main(){if true{fmt.Println("TRUE");if true{fmt.Println("TRUE")}};if false{fmt.Println("TRUE")}else{fmt.Println("FALSE")};if false{fmt.Println("TRUE")}else if true{fmt.Println("TRUE")};if err:=run1();err!=nil{fmt.Println(err)};if _,err:=run2();err!=nil{fmt.Println(err)}};func run1()error{return nil};func run2()(string,error){return "",nil};`,
+		},
+		{
+			name: "inc dec",
+			src: `package main
+
+import "fmt"
+
+func main() {
+	i := 1
+	i++
+	i--
+	i++
+	fmt.Println(i)
+}
+`,
+			want: `package main;import "fmt";func main(){i:=1;i++;i--;i++;fmt.Println(i)};`,
 		},
 	}
 
