59f325d81ddbf2c84b0f33c7aebea4ab9ce116be
koki-develop <kou.pg.0131@gmail.com>
2024-03-05 09:54:52 +0900

    Add support for additional expression types in stringifyExpr function

--- internal/mingo/expr.go
+++ internal/mingo/expr.go
@@ -42,6 +42,140 @@ func stringifyExpr(expr ast.Expr) string {
 	case *ast.Ellipsis:
 		sb.WriteString("...")
 		sb.WriteString(stringifyExpr(x.Elt))
+	case *ast.FuncLit:
+		// TODO
+	case *ast.BinaryExpr:
+		sb.WriteString(stringifyExpr(x.X))
+		sb.WriteString(x.Op.String())
+		sb.WriteString(stringifyExpr(x.Y))
+	case *ast.SliceExpr:
+		sb.WriteString(stringifyExpr(x.X))
+		sb.WriteString("[")
+		if x.Low != nil {
+			sb.WriteString(stringifyExpr(x.Low))
+		}
+		sb.WriteString(":") // FIXME
+		if x.High != nil {
+			sb.WriteString(stringifyExpr(x.High))
+		}
+		if x.Max != nil {
+			sb.WriteString(":")
+			sb.WriteString(stringifyExpr(x.Max))
+		}
+		sb.WriteString("]")
+	case *ast.UnaryExpr:
+		sb.WriteString(x.Op.String())
+		sb.WriteString(stringifyExpr(x.X))
+	case *ast.CompositeLit:
+		sb.WriteString(stringifyExpr(x.Type))
+		sb.WriteString("{")
+		for i, elt := range x.Elts {
+			if i > 0 {
+				sb.WriteString(",")
+			}
+			sb.WriteString(stringifyExpr(elt))
+		}
+		sb.WriteString("}")
+	case *ast.ParenExpr:
+		sb.WriteString("(")
+		sb.WriteString(stringifyExpr(x.X))
+		sb.WriteString(")")
+	case *ast.IndexExpr:
+		sb.WriteString(stringifyExpr(x.X))
+		sb.WriteString("[")
+		sb.WriteString(stringifyExpr(x.Index))
+		sb.WriteString("]")
+	case *ast.KeyValueExpr:
+		sb.WriteString(stringifyExpr(x.Key))
+		sb.WriteString(":")
+		sb.WriteString(stringifyExpr(x.Value))
+	case *ast.TypeAssertExpr:
+		sb.WriteString(stringifyExpr(x.X))
+		sb.WriteString(".(")
+		sb.WriteString(stringifyExpr(x.Type))
+		sb.WriteString(")")
+	case *ast.ChanType:
+		if x.Dir == ast.RECV {
+			sb.WriteString("<-")
+		}
+		sb.WriteString("chan")
+		sb.WriteString(stringifyExpr(x.Value))
+	case *ast.MapType:
+		sb.WriteString("map[")
+		sb.WriteString(stringifyExpr(x.Key))
+		sb.WriteString("]")
+		sb.WriteString(stringifyExpr(x.Value))
+	case *ast.InterfaceType:
+		sb.WriteString("interface{}")
+	case *ast.StructType:
+		sb.WriteString("struct{")
+		for i, field := range x.Fields.List {
+			if i > 0 {
+				sb.WriteString(";")
+			}
+			for j, name := range field.Names {
+				if j > 0 {
+					sb.WriteString(",")
+				}
+				sb.WriteString(name.Name)
+			}
+			sb.WriteString(" ")
+			sb.WriteString(stringifyExpr(field.Type))
+		}
+		sb.WriteString("}")
+	case *ast.FuncType:
+		sb.WriteString("func(")
+
+		// args
+		for i, arg := range x.Params.List {
+			if i > 0 {
+				sb.WriteString(",")
+			}
+			for j, name := range arg.Names {
+				if j > 0 {
+					sb.WriteString(",")
+				}
+				sb.WriteString(name.Name)
+			}
+			sb.WriteString(" ")
+			sb.WriteString(stringifyExpr(arg.Type))
+		}
+		sb.WriteString(")")
+
+		// result
+		if x.Results != nil {
+			rb := new(strings.Builder)
+
+			needParens := false
+			if len(x.Results.List) > 1 {
+				needParens = true
+			}
+
+			for i, rslt := range x.Results.List {
+				if i > 0 {
+					rb.WriteString(",")
+				}
+
+				for j, name := range rslt.Names {
+					needParens = true
+					if j > 0 {
+						rb.WriteString(",")
+					}
+					rb.WriteString(name.Name)
+					rb.WriteString(" ")
+				}
+
+				rb.WriteString(stringifyExpr(rslt.Type))
+			}
+
+			if needParens {
+				sb.WriteString("(")
+			}
+			sb.WriteString(rb.String())
+			if needParens {
+				sb.WriteString(")")
+			}
+		}
 	default:
 		sb.WriteString(x.(*ast.Ident).Name)
 	}
--- internal/mingo/func.go
+++ internal/mingo/func.go
@@ -16,58 +16,7 @@ func minifyFuncDecl(n *ast.FuncDecl) string {
 	}
 
 	fmt.Fprintf(sb, "%s(", n.Name.Name)
-
-	// args
-	for i, arg := range n.Type.Params.List {
-		if i > 0 {
-			sb.WriteString(",")
-		}
-		for j, name := range arg.Names {
-			if j > 0 {
-				sb.WriteString(",")
-			}
-			sb.WriteString(name.Name)
-		}
-
-		sb.WriteString(" ")
-		sb.WriteString(stringifyExpr(arg.Type))
-	}
-	sb.WriteString(")")
-
-	// result
-	if n.Type.Results != nil {
-		rb := new(strings.Builder)
-
-		needParens := false
-		if len(n.Type.Results.List) > 1 {
-			needParens = true
-		}
-
-		for i, rslt := range n.Type.Results.List {
-			if i > 0 {
-				rb.WriteString(",")
-			}
-
-			for j, name := range rslt.Names {
-				needParens = true
-				if j > 0 {
-					rb.WriteString(",")
-				}
-				rb.WriteString(name.Name)
-				rb.WriteString(" ")
-			}
-
-			rb.WriteString(stringifyExpr(rslt.Type))
-		}
-
-		if needParens {
-			sb.WriteString("(")
-		}
-		sb.WriteString(rb.String())
-		if needParens {
-			sb.WriteString(")")
-		}
-	}
+	sb.WriteString(stringifyExpr(n.Type))
 
 	// body
 	sb.WriteString("{")
