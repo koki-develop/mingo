daaefdd9038e4089db1964d82fcc6a54cd69c391
koki-develop <kou.pg.0131@gmail.com>
2024-03-05 21:44:59 +0900

    Update struct test case

--- internal/mingo/mingo_test.go
+++ internal/mingo/mingo_test.go
@@ -132,8 +132,19 @@ type S2 struct {
 	F5 func(string)
 	F6 func(s1, s2 string)
 	F7 func(f1 func() string)
-}`,
-			want: `package main;type S1 struct{S struct{A string;B int;C bool};F1 func()string;F2 func()(string,error);F3 func()(s string,err error)};type S2 struct{S1;A string;B int;C bool;a string;b int;c bool;F4 func(s string);F5 func(string);F6 func(s1,s2 string);F7 func(f1 func()string)};`,
+}
+
+func main() {
+	type S3 struct {
+		S1
+
+		A string
+		B int
+		C bool
+	}
+}
+`,
+			want: `package main;type S1 struct{S struct{A string;B int;C bool};F1 func()string;F2 func()(string,error);F3 func()(s string,err error)};type S2 struct{S1;A string;B int;C bool;a string;b int;c bool;F4 func(s string);F5 func(string);F6 func(s1,s2 string);F7 func(f1 func()string)};func main(){type S3 struct{S1;A string;B int;C bool};};`,
 		},
 		{
 			name: "interface",
