c542adfe5062ff1d62a312e44bb6c19c7e34f971
koki-develop <kou.pg.0131@gmail.com>
2024-03-05 23:09:18 +0900

    Add --write flag

--- cmd/root.go
+++ cmd/root.go
@@ -2,26 +2,48 @@ package cmd
 
 import (
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/koki-develop/mingo/internal/mingo"
 	"github.com/spf13/cobra"
 )
 
+var (
+	flagWrite bool
+)
+
 var rootCmd = &cobra.Command{
 	Use: "mingo",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		for i, file := range args {
-			if i > 0 {
-				fmt.Println()
-			}
-
-			min, err := mingo.MinifyFile(file)
+			src, err := os.ReadFile(file)
 			if err != nil {
 				return err
 			}
 
-			fmt.Print(min)
+			var out io.Writer
+			if flagWrite {
+				f, err := os.Create(file)
+				if err != nil {
+					return err
+				}
+				defer f.Close()
+
+				out = f
+			} else {
+				out = os.Stdout
+				if i > 0 {
+					fmt.Fprintln(out)
+				}
+			}
+
+			min, err := mingo.Minify(file, src)
+			if err != nil {
+				return err
+			}
+
+			fmt.Fprint(out, min)
 		}
 
 		return nil
@@ -32,4 +54,8 @@ func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		os.Exit(1)
 	}
+}
+
+func init() {
+	rootCmd.Flags().BoolVarP(&flagWrite, "write", "w", false, "write result to (source) file instead of stdout")
 }
